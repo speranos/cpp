af679983bd761d3933b9e10dea47bcefb595d33b
enimv <oueldmamaamine@gmail.com>
2023-04-09 18:09:11 +0000

    enima

---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,17 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+	protected:
+	std::string type;
+    public:
+    Animal();
+    Animal(Animal &copy);
+    ~Animal();
+	std::string getType();
+};
+
+#endif
