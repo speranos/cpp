e4f97f68c407bd98719cf93a891824e1bdad8895
speranos <aoueldma@student.1337.ma>
2023-04-27 00:51:57 +0100

    reinterpret_cast

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -13,12 +13,12 @@ Serializer::~Serializer()
 uintptr_t Serializer::serialize(Data* ptr)
 {
 	//change cast model
-	return((uintptr_t)ptr);
+	return(reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data* Serializer::deserialize(uintptr_t raw)
 {
 	//change cast model
-	return((Data*)raw);
+	return(reinterpret_cast<Data*>(raw));
 }
 
